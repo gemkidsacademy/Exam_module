d8e01d4798d1913a29dfc3e27c9bff5ac1c7fd88
gemkidsacademy <info@gemkidsacademy.com.au>
2025-11-28 11:52:00 +0500

    Update ViewUserModal.js

--- src/components/ViewUserModal.js
+++ src/components/ViewUserModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./AddUserForm.css"; // reuse modal styling
+import "./AddStudentForm.css"; // reuse modal styling
 
 function ViewUserModal({ onClose }) {
   const [users, setUsers] = useState([]);
