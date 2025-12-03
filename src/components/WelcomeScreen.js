410ed072ead083ef2c841189b000308c8e398f0b
gemkidsacademy <info@gemkidsacademy.com.au>
2025-12-03 12:04:34 +0500

    Create WelcomeScreen.js

---
+++ src/components/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WelcomeScreen = () => {
+  return (
+    <div className="welcome-screen">
+      <h2>Welcome to the Selective Dashboard</h2>
+      <p>Please select a subject above to get started.</p>
+    </div>
+  );
+};
+
+export default WelcomeScreen;
