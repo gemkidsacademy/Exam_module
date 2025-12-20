06779c6796ac6743a8ef229697bb70a0a3c7dfbd
gemkidsacademy <info@gemkidsacademy.com.au>
2025-12-20 11:52:05 +0500

    Update ExamTypeSelector_generate_exam.js

--- src/components/ExamTypeSelector_generate_exam.js
+++ src/components/ExamTypeSelector_generate_exam.js
@@ -1,20 +1,20 @@
 import React from "react";
 import GenerateExam from "./GenerateExam";
 import GenerateExam_reading from "./GenerateExam_reading";
-import GenerateExam_foundational from "./GenerateExam_foundational";
+import GenerateExam_math_reasoning from "./GenerateExam_math_reasoning";
 import GenerateExam_writing from "./GenerateExam_writing";
 import "./ExamTypeSelectorGenerate.css";
 
 export default function ExamTypeSelector_generate_exam({ examType, onSelect }) {
   const examOptions = [
     { label: "Thinking Skills Exam", value: "thinkingskills" },
-    { label: "Foundational Exam", value: "foundational" },
+    { label: "Mathematical Reasoning Exam", value: "math_reasoning" },
     { label: "Reading Exam", value: "reading" },
     { label: "Writing Exam", value: "writing" },
   ];
 
   if (examType === "thinkingskills") return <GenerateExam />;
-  if (examType === "foundational") return <GenerateExam_foundational />;
+  if (examType === "math_reasoning") return <GenerateExam_math_reasoning />;
   if (examType === "reading") return <GenerateExam_reading />;
   if (examType === "writing") return <GenerateExam_writing />;
 
