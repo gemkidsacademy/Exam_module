64e0fb00709148d6c323a7a5f45b1e06c9c00f85
gemkidsacademy <info@gemkidsacademy.com.au>
2025-12-21 12:52:29 +0500

    Update ExamPageThinkingSkills.js

--- src/components/ExamPageThinkingSkills.js
+++ src/components/ExamPageThinkingSkills.js
@@ -133,7 +133,7 @@ export default function ExamPageThinkingSkills() {
 
       try {
         await fetch(
-          "https://web-production-481a5.up.railway.app/api/student/finish-exam",
+          "https://web-production-481a5.up.railway.app/api/student/finish-exam-thinkingskills",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
