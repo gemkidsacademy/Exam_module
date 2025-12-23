cb5ccb1004f15cf4ef40675ed23cefa9662d3d07
gemkidsacademy <info@gemkidsacademy.com.au>
2025-12-23 14:42:03 +0500

    Create StudentExamReports.js

---
+++ src/components/StudentExamReports.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function StudentExamReports() {
+  return (
+    <div>
+      <h2>Student Exam Reports</h2>
+      <p>Select a student to view Selective exam reports.</p>
+    </div>
+  );
+}
