a0cc1f7ac3136f23d7824e13d0eb4ed82c9e24a9
gemkidsacademy <info@gemkidsacademy.com.au>
2026-01-07 12:49:25 +0500

    Create mockProgressData.js

---
+++ src/components/mockProgressData.js
@@ -0,0 +1,19 @@
+export const mockSameExamProgress = {
+  thinking_skills: [
+    { date: "2024-01-10", score: 40, accuracy: 38 },
+    { date: "2024-02-15", score: 52, accuracy: 50 },
+    { date: "2024-03-20", score: 65, accuracy: 62 }
+  ],
+
+  reading: [
+    { date: "2024-01-12", score: 60, accuracy: 58 },
+    { date: "2024-02-18", score: 63, accuracy: 61 },
+    { date: "2024-03-22", score: 66, accuracy: 64 }
+  ],
+
+  writing: [
+    { date: "2024-01-15", score: 10, accuracy: 50 }, // out of 20
+    { date: "2024-02-20", score: 12, accuracy: 60 },
+    { date: "2024-03-25", score: 14, accuracy: 70 }
+  ]
+};
