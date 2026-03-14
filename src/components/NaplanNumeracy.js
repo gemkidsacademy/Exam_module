f73d472c59e07b39dda9f503a985cca03c955254
gemkidsacademy <info@gemkidsacademy.com.au>
2026-03-14 12:50:49 +0500

    Update NaplanNumeracy.js

--- src/components/NaplanNumeracy.js
+++ src/components/NaplanNumeracy.js
@@ -208,7 +208,11 @@
   
       startExam();
     }, [studentId, API_BASE, loadReport, mode, onExamStart]);
-  
+   useEffect(() => {
+  document.addEventListener("contextmenu", e => e.preventDefault());
+  document.addEventListener("copy", e => e.preventDefault());
+  document.addEventListener("cut", e => e.preventDefault());
+}, []);
     /* ============================================================
        FINISH EXAM
     ============================================================ */
