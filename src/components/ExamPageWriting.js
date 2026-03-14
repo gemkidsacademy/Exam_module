3640f46ef23904c9d902b77a79928d894cf68092
gemkidsacademy <info@gemkidsacademy.com.au>
2026-03-14 13:11:06 +0500

    Update ExamPageWriting.js

--- src/components/ExamPageWriting.js
+++ src/components/ExamPageWriting.js
@@ -182,12 +182,7 @@
 
   return () => clearInterval(timer);
 }, [timeLeft, loading, completed, submitting, showConfirmFinish]);
-    useEffect(() => {
-  document.addEventListener("contextmenu", e => e.preventDefault());
-  document.addEventListener("copy", e => e.preventDefault());
-  document.addEventListener("cut", e => e.preventDefault());
-}, []);
-
+    
     useEffect(() => {
   if (
     examActive &&
