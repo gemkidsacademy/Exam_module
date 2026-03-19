2ebf189cdfed19131cb80419857a2f427d56c3a0
gemkidsacademy <info@gemkidsacademy.com.au>
2026-03-19 13:04:25 +0500

    Update QuizSetup_oc_reading.js

--- src/components/QuizSetup_oc_reading.js
+++ src/components/QuizSetup_oc_reading.js
@@ -183,7 +183,7 @@ export default function QuizSetup_oc_reading() {
       });
 
       const res = await fetch(
-        `https://web-production-481a5.up.railway.app/api/reading/question-bank?${params}`
+        `https://web-production-481a5.up.railway.app/api/reading/question-bank-oc?${params}`
       );
 
       if (!res.ok) throw new Error("Failed");
