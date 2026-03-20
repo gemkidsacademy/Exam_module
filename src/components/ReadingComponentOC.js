fbf79f1da29464709aaada1975a41c5647035e9c
gemkidsacademy <info@gemkidsacademy.com.au>
2026-03-20 15:46:18 +0500

    Update ReadingComponentOC.js

--- src/components/ReadingComponentOC.js
+++ src/components/ReadingComponentOC.js
@@ -358,7 +358,7 @@ console.log("✅ FLATTENED QUESTIONS COUNT:", flatQuestions.length);
 
   try {
     const res = await fetch(
-      `${API_BASE}/api/exams/submit-oc-reading-new`,
+      `${API_BASE}/api/exams/submit-oc-reading`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
