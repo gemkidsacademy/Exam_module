aecc6d7fb0949d2196737443b4cb061c185a7841
gemkidsacademy <info@gemkidsacademy.com.au>
2026-03-20 15:45:56 +0500

    Update ReadingComponentOC.js

--- src/components/ReadingComponentOC.js
+++ src/components/ReadingComponentOC.js
@@ -95,7 +95,7 @@ export default function ReadingComponentOC({
 
   try {
     const res = await fetch(
-      `${API_BASE}/api/exams/review-oc-reading?session_id=${attemptId}`
+      `${API_BASE}/api/exams/review-oc-reading-new?session_id=${attemptId}`
     );
 
     if (!res.ok) {
