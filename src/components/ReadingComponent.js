fdd843ed80ac6f34525aa76515af9f5f35f2b80c
gemkidsacademy <info@gemkidsacademy.com.au>
2026-03-24 11:14:54 +0500

    Update ReadingComponent.js

--- src/components/ReadingComponent.js
+++ src/components/ReadingComponent.js
@@ -202,11 +202,7 @@ export default function ReadingComponent({
     questionsCount: questions.length
   });
 }, [exam, questions]);
-    useEffect(() => {
-  document.addEventListener("contextmenu", e => e.preventDefault());
-  document.addEventListener("copy", e => e.preventDefault());
-  document.addEventListener("cut", e => e.preventDefault());
-}, []);
+    
   //useEffect(() => {
   //document.addEventListener("contextmenu", e => e.preventDefault());
   //document.addEventListener("copy", e => e.preventDefault());
