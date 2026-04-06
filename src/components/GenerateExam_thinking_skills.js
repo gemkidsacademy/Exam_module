82574aa3390a5e4790cdb7ed1e261cb5a074026a
gemkidsacademy <info@gemkidsacademy.com.au>
2026-04-06 18:34:15 +0500

    Update GenerateExam_thinking_skills.js

--- src/components/GenerateExam_thinking_skills.js
+++ src/components/GenerateExam_thinking_skills.js
@@ -93,7 +93,7 @@ export default function GenerateExam_thinking_skills() {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
-            class_year: quiz.classYear
+            class_year: selectedClassYear
           }),
         }
       );
