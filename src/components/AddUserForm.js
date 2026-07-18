5ac190b4e9bc71295634b6b7e5755747f6eec9c4
Sajjad Ali Noor <proactive1.san@gmail.com>
2026-07-18 13:37:18 +0500

    ok

--- src/components/AddUserForm.js
+++ src/components/AddUserForm.js
@@ -24,14 +24,18 @@ export default function AddStudentForm() {
   const [studentYear, setStudentYear] = useState("");
   const STUDENT_YEAR_OPTIONS = [
         "Kindergarten",
+        "Year 1",
         "Year 2",
         "Year 3",    
         "Year 4",
         "Year 5",
         "Year 6",       
         "Year 7",
+        "Year 8",
         "Year 9",
-        "Year 10",  
+        "Year 10",
+        "Year 11",
+        "Year 12",  
       ];
   const CLASS_NAME_OPTIONS = [
   "NAPLAN",
